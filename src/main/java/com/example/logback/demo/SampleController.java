3e62515ffa9fb7993668a4651af94847d4430204
masanobu.uenaka <masanobu.uenaka@asoview.co.jp>
2019-09-13 11:47:08 +0900

    first commit

---
+++ src/main/java/com/example/logback/demo/SampleController.java
@@ -0,0 +1,27 @@
+package com.example.logback.demo;
+
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    Logger loggerA = org.slf4j.LoggerFactory.getLogger("com.hoge");
+    Logger loggerB = org.slf4j.LoggerFactory.getLogger(getClass().getPackage().getName());
+
+
+    @RequestMapping("/")       // URLのパスの指定
+    private String top() { // リクエストを受け付けるメソッド
+
+        loggerA.info("これはrootロガーのみ継承されているためFILE_Aのみに出力されます。");
+        loggerB.info("これはcom.exampleロガーを継承しているのでFILE_AとFILE_Bに出力されます。");
+
+        return "/index.html";
+    }
+}
