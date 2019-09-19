6e307b469f245a6f6b0c4dd3ec88898b24c6ee47
masanobu.uenaka <masanobu.uenaka@asoview.co.jp>
2019-09-19 19:37:38 +0900

    指摘対応

--- src/main/java/com/example/logback/demo/SampleController.java
+++ src/main/java/com/example/logback/demo/SampleController.java
@@ -3,6 +3,7 @@ package com.example.logback.demo;
 import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -15,12 +16,12 @@ public class SampleController {
     Logger loggerB = org.slf4j.LoggerFactory.getLogger(getClass().getPackage().getName());
 
 
-    @RequestMapping("/")       // URLのパスの指定
+    @GetMapping("/")       // URLのパスの指定
     private String top() { // リクエストを受け付けるメソッド
 
         loggerA.info("これはrootロガーのみ継承されているためFILE_Aのみに出力されます。");
         loggerB.info("これはcom.exampleロガーを継承しているのでFILE_AとFILE_Bに出力されます。");
 
-        return "/index.html";
+        return "/index";
     }
 }
