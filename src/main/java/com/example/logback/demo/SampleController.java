3566679135cb81f3b5c55c078f7fc4d6ab33274a
masanobu.uenaka <masanobu.uenaka@asoview.co.jp>
2019-09-19 19:58:30 +0900

    不要なimport削除

--- src/main/java/com/example/logback/demo/SampleController.java
+++ src/main/java/com/example/logback/demo/SampleController.java
@@ -1,13 +1,8 @@
 package com.example.logback.demo;
 
 import org.slf4j.Logger;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class SampleController {
